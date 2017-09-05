714e434fac3f2f156e5eba224f4670738216cbe5
Richard Weber <riche.weber@gmail.com>
2017-09-05 13:58:22 +0300

    init

---
+++ src/main/java/com/example/demo/TextObject.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+class TextObject {
+    private String value;
+
+    TextObject(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
